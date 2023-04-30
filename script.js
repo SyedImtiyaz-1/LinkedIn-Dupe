9a39a5353aa9747fae793c4f81a392bb51f3bbd5
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-01 02:18:17 +0530

    added

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let openPost = document.querySelector(".startpost");
+let closePost = document.getElementById('closepop');
+
 function construction() {
     alert('Under Construction');
 }
-
 setTimeout(construction, 3000);
+
+
+openPost.addEventListener("click", function () {
+  let openBox = document.querySelector(".popup");
+  openBox.style.display="block";
+});
+
+closePost.addEventListener('click', function(){
+    let closeBox = document.querySelector(".popup");
+    closeBox.style.display="none";
+})
+
