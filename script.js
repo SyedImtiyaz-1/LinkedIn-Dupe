232ed1b1f388fd37fdab3eec876436a47c5029f8
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-04 12:40:30 +0530

    added

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-let openPost = document.querySelector(".startpost");
-let closePost = document.getElementById('closepop');
+// let openPost = document.querySelector(".startpost");
+// let closePost = document.getElementById('closepop');
 
-function construction() {
-    alert('Under Construction');
-}
-setTimeout(construction, 3000);
+// function construction() {
+//     alert('Under Construction');
+// }
+// setTimeout(construction, 3000);
 
 
-openPost.addEventListener("click", function () {
-  let openBox = document.querySelector(".popup");
-  openBox.style.display="block";
-});
+// openPost.addEventListener("click", function () {
+//   let openBox = document.querySelector(".popup");
+//   openBox.style.display="block";
+// });
 
-closePost.addEventListener('click', function(){
-    let closeBox = document.querySelector(".popup");
-    closeBox.style.display="none";
-})
-
+// closePost.addEventListener('click', function(){
+//     let closeBox = document.querySelector(".popup");
+//     closeBox.style.display="none";
+// })
